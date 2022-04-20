83d6cedc0b38f995e4fe6d2c92e7ed73795fa544
Brant Martin <brantmartin473@gmail.com>
2022-04-19 19:57:33 -0700

    debugged routes and added delete to api routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,20 +3,36 @@ const fs = require("fs");
 let api = require("../db/db.json");
 
 app.get("/api/notes", function (req, res) {
-  api = JSON.parse(fs.readFileSync("/db/db.json")) || [];
+  api = JSON.parse(fs.readFileSync("./db/db.json")) || [];
   res.json(api);
 });
 
 app.post("/api/notes", function (req, res) {
-  api.push([
+  api.push(
     {
       id: Math.floor(Math.random() * 100),
       title: req.body.title,
       text: req.body.text,
     },
-  ]);
-  fs.rwriteFileSync("/db/db.json", JSON.stringify(api), function (err) {
+  );
+  fs.writeFileSync("./db/db.json", JSON.stringify(api), function (err) {
     if (err) throw err;
   });
   res.json(api);
 });
+
+app.delete("/api/notes/:id", function (req, res) { 
+    let notDeletedNotes = []
+    for (let i = 0; i < api.length; i++) {
+        if (api[i].id != req.params.id){
+            notDeletedNotes.push(api[i])
+        }
+    }
+    api = notDeletedNotes
+    fs.writeFileSync("./db/db.json", JSON.stringify(api), function (err) {
+      if (err) throw err;
+    });
+    res.json(api);
+  });
+
+module.exports = app
--- routes/html_routes.js
+++ routes/html_routes.js
@@ -14,4 +14,4 @@ app.get("*",function(req,res){
     res.sendFile(path.join(__dirname,"../public/index.html"))
 })
 
-module.export = app
+module.exports = app
