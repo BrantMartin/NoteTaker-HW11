306059908489bda188d825b21049aa10852cb831
Brant Martin <brantmartin473@gmail.com>
2022-05-10 18:34:15 -0700

    commit for turn in

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static('public'))
-
+app.use(require("./routes/api_routes"))
+app.use(require("./routes/html_routes"))
 
 app.listen(PORT, function (){
     console.log('Connected to PORT');
-})
+});
